e0e45b7d9e590d93a990f77ee129ef2a5b2b14fa
Diogo Filipe <diogopd@hotmail.com>
2020-06-02 19:01:05 -0300

    criando botão minúsculo

--- manifest/script.js
+++ manifest/script.js
@@ -3,10 +3,20 @@ document.addEventListener('DOMContentLoaded',function(){
     
     document.querySelector('.botaoMa').addEventListener('click',function(){
         let text = document.querySelector('#texto').value;
-        document.querySelector("#resultado").innerHTML = upper(text);
+        document.querySelector("#text-resultado").innerHTML = upper(text);
     })
-
     function upper (text){
         return text.toUpperCase();
     }
+
+    document.querySelector('.botaoMi').addEventListener('click',function(){
+        let textM = document.querySelector('#texto').value;
+        document.querySelector("#text-resultado").innerHTML = lower(textM);
+    })
+
+    function lower (textM){
+        return textM.toLowerCase();
+    }
 })
+upper(text);
+lower(textM);
